38a90910ef3c5200160db28a3b5eefda27601b9a
Tomonori Matsukawa <maccho.chekera@gmail.com>
2019-10-06 19:07:32 +0900

    mysql env

--- app/main.go
+++ app/main.go
@@ -6,12 +6,25 @@ import (
 	"net/http"
 	"time"
 
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-var tmpl = template.Must(template.ParseFiles("base.html"))
+var tmpl = template.Must(template.ParseFiles("./template/base.html"))
+var tmpl2 = template.Must(template.ParseFiles("./template/list.html"))
+
+type Book struct {
+	Id                     int
+	Title                  string
+	Author                 string
+	Latest_Issue           float32
+	Front_Cover_Image_Path string
+}
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// テンプレートに埋め込むデータ作成
 	dat := struct {
 		Title string
 		Time  time.Time
@@ -19,16 +32,47 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		Title: "Test",
 		Time:  time.Now(),
 	}
+	// テンプレートにデータを埋め込む
 	if err := tmpl.ExecuteTemplate(w, "base.html", dat); err != nil {
 		log.Fatal(err)
 	}
+
 }
 
+func dbHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "racmy:racmy@tcp(db:3306)/book-management")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close() // 関数がリターンする直前に呼び出される
+
+	rows, err := db.Query("SELECT * FROM book")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Bookを格納するArray作成
+	var books = []Book{}
+	for rows.Next() {
+		var book Book
+		err = rows.Scan(&book.Id, &book.Title, &book.Author, &book.Latest_Issue, &book.Front_Cover_Image_Path)
+		log.Print(book.Title)
+		if err != nil {
+			panic(err)
+		}
+		log.Print(book.Title)
+		books = append(books, book)
+	}
+
+	if err := tmpl2.ExecuteTemplate(w, "list.html", books); err != nil {
+		log.Fatal(err)
+	}
+}
 func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", rootHandler)
-
+	r.HandleFunc("/db", dbHandler)
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
 }
---
+++ mysql/init.sql
@@ -0,0 +1,14 @@
+use book-management;
+
+CREATE TABLE book (
+  id int(10) unsigned not null auto_increment,
+  title varchar(128) not null,
+  author varchar(128),
+  latest_issue float unsigned not null default 1,
+  front_cover_image_path varchar(128) not null default "img/no_image.jpeg",
+  primary key (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO book (id, title, author, latest_issue) VALUES (1, 'ジョジョの奇妙な冒険', '荒木比呂彦', 25);
+INSERT INTO book (id, title, author, latest_issue) VALUES (2, 'ナルト', '岸本 斉史', 45);
+INSERT INTO book (id, title, author, latest_issue) VALUES (3, 'テニスの王子様', '許斐剛', 21);
